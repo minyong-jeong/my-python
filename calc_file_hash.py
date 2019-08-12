3221f5b05ee38dbbf52cbb9d3ec0c6b7be57f4f3
Minyong Jeong <jmy3155@gmail.com>
2019-08-13 00:07:40 +0900

    Create calc_file_hash.py

---
+++ calc_file_hash.py
@@ -0,0 +1,20 @@
+import hashlib
+
+def calc_file_hash(path):
+    """
+    Calculate file hash.
+
+    Args:
+        path (str): file path for calculating hash.
+
+    Returns:
+        str: file hash
+    """
+    f = open(path, 'rb')
+    data = f.read()
+    hash = hashlib.md5(data).hexdigest()
+    return hash
+
+if __name__ == "__main__":
+    hash_val = calc_file_hash("./README.md")
+    print(hash_val)
