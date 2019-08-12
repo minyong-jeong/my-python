101e2b7f291a6d20be9c7c0b418dd0ed2f9bc161
Minyong Jeong <jmy3155@gmail.com>
2019-08-13 00:07:45 +0900

    Update json_util.py

--- json_util.py
+++ json_util.py
@@ -1,20 +1,12 @@
 import json
 
-def parse_json(json_data):
-    """Convert json to dict"""
-    dict_data = json.loads(json_data)
-    return dict_data
+json_data = '{ "p1": { "name": "chulsu", "age": 20 }, "p2": { "name": "younghee", "age": 22 } }'
+dict_data = { "p1": { "name": "chulsu", "age": 20 }, "p2": { "name": "younghee", "age": 22 } }
 
-def convert_json(dict_data):
-    """Convert dict to json"""
-    json_data = json.dumps(dict_data)
-    return json_data
+# Convert json to dict
+result = json.loads(json_data)  
+print("parse_json result: %s" % type(result))
 
-if __name__ == "__main__":
-    json_data = '{ "p1": { "name": "chulsu", "age": 20 }, "p2": { "name": "younghee", "age": 22 } }'
-    result = parse_json(json_data)
-    print("parse_json result: %s" % type(result))
-
-    dict_data = { "p1": { "name": "chulsu", "age": 20 }, "p2": { "name": "younghee", "age": 22 } }
-    result = convert_json(dict_data)
-    print("convert_json result: %s" % type(result))
+# Convert dict to json
+result = json.dumps(dict_data) 
+print("convert_json result: %s" % type(result))
