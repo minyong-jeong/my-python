a5ee6ed301bf3131311c6515a59844e003fac402
Minyong Jeong <jmy3155@gmail.com>
2019-08-13 00:07:50 +0900

    Update make_dir.py

--- make_dir.py
+++ make_dir.py
@@ -1,11 +1,3 @@
 import os
 
-def make_dir(path):
-    try:
-        os.makedirs(path)
-    except Exception as e:
-        print(e)
-
-if __name__ == "__main__":
-    make_dir("./temp")
-
+os.makedirs("./temp")
