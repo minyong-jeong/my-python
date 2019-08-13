76fee3e05ad4c70fd108e621a6a48cf57e1d85cc
Minyong Jeong <jmy3155@gmail.com>
2019-08-13 20:49:09 +0900

    Update json_util.py

--- json_util.py
+++ json_util.py
@@ -4,9 +4,9 @@ json_data = '{ "p1": { "name": "chulsu", "age": 20 }, "p2": { "name": "younghee"
 dict_data = { "p1": { "name": "chulsu", "age": 20 }, "p2": { "name": "younghee", "age": 22 } }
 
 # Convert json to dict
-result = json.loads(json_data)  
+result = json.loads(json_data)
 print("parse_json result: %s" % type(result))
 
 # Convert dict to json
-result = json.dumps(dict_data) 
+result = json.dumps(dict_data)
 print("convert_json result: %s" % type(result))
