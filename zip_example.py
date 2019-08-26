d563ffeceb71999991afc1dd092b1b1a0c7c78ab
Minyong Jeong <jmy3155@gmail.com>
2019-08-26 13:20:03 +0900

    Update zip_example.py

--- zip_example.py
+++ zip_example.py
@@ -2,28 +2,19 @@ import os
 import zipfile
 
 def dir_zip(dir_path, dest_path):
-    try:
-        if not os.path.isdir(dir_path):
-            raise Exception("No such directory: '%s'" % dir_path)
+    with zipfile.ZipFile(dest_path, "w") as zf:
+        for (path, dir, files) in os.walk(dir_path):
+            for file in files:
+                fullpath = os.path.join(path, file)
+                relpath = os.path.relpath(fullpath, dir_path)
+                zf.write(fullpath, relpath, zipfile.ZIP_DEFLATED)
+        zf.close()
 
-        with zipfile.ZipFile(dest_path, "w") as zf:
-            for (path, dir, files) in os.walk(dir_path):
-                for file in files:
-                    fullpath = os.path.join(path, file)
-                    relpath = os.path.relpath(fullpath, dir_path)
-                    zf.write(fullpath, relpath, zipfile.ZIP_DEFLATED)
-            zf.close()
-    except Exception as e:
-        print("[ERROR] >>> %s" % e)
-        return False
-    return True
+def extract_zip(zip_path, dest_path):
+    with zipfile.ZipFile(zip_path) as zf:
+        zf.extractall(dest_path)
+        zf.close()
 
-def extract_zip(target_zip, dest_path):
-    try:
-        with zipfile.ZipFile(target_zip) as zf:
-            zf.extractall(dest_path)
-            zf.close()
-    except Exception as e:
-        print("[ERROR] >>> %s" % e)
-        return False
-    return True
+if __name__ == "__main__":
+    dir_zip('/path/to/dir', '/path/to/<filename>.zip')
+    extract_zip('/path/to/zip_file', '/path/to/dir')
