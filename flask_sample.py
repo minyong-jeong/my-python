367b43acb42a89ab39c4e249e2659b46fa5b4b8c
Minyong Jeong <jmy3155@gmail.com>
2019-11-04 22:21:01 +0900

    Update flask_sample.py (Add post request)

--- flask_sample.py
+++ flask_sample.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/', methods=['POST', 'GET'])
+def hello():
+    if request.method == 'POST':
+        data = request.json
+        return data
+    else: 
+        return 'Hello (GET)'
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0')
+   app.run(host='0.0.0.0', port=5000)
