d2bd042e01f42bc218064f4bbffbf5789f5adb64
Minyong Jeong <jmy3155@gmail.com>
2019-11-26 23:00:09 +0900

    Update ssh_connect.py

--- ssh_connect.py
+++ ssh_connect.py
@@ -1,5 +1,5 @@
 import paramiko
-from scp import SCPClient
+from scp import SCPClient, SCPException
 
 class SSHManager:
     """
@@ -8,35 +8,47 @@ class SSHManager:
         >>> ssh_manager = SSHManager()
         >>> ssh_manager.create_ssh_client(hostname, username, password)
         >>> ssh_manager.send_command("ls -al")
-        >>> ssh_manager.send_file("/path/to/local_file", "/path/to/server_dir")
-        >>> ssh_manager.get_file("/path/to/server_file", "/path/to/local_dir")
+        >>> ssh_manager.send_file("/path/to/local_path", "/path/to/remote_path")
+        >>> ssh_manager.get_file("/path/to/remote_path", "/path/to/local_path")
         ...
         >>> ssh_manager.close_ssh_client()
     """
-
     def __init__(self):
-        pass
+        self.ssh_client = None
 
     def create_ssh_client(self, hostname, username, password):
-        self.ssh_client = paramiko.SSHClient()
-        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh_client.connect(hostname, username=username, password=password)
+        """Create SSH client session to remote server"""
+        if self.ssh_client is None:
+            self.ssh_client = paramiko.SSHClient()
+            self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.ssh_client.connect(hostname, username=username, password=password)
+        else:
+            print("SSH client session exist.")
 
     def close_ssh_client(self):
+        """Close SSH client session"""
         self.ssh_client.close()
 
-    def send_file(self, file_path, server_dir):
-        with SCPClient(self.ssh_client.get_transport()) as scp:
-            scp.put(file_path, server_dir, preserve_times=True)
+    def send_file(self, local_path, remote_path):
+        """Send a single file to remote path"""
+        try:
+            with SCPClient(self.ssh_client.get_transport()) as scp:
+                scp.put(local_path, remote_path, preserve_times=True)
+        except SCPException:
+            raise SCPException.message
 
-    def get_file(self, server_path, local_dir):
-        with SCPClient(self.ssh_client.get_transport()) as scp:
-            scp.get(server_path, local_dir)
+    def get_file(self, remote_path, local_path):
+        """Get a single file from remote path"""
+        try:
+            with SCPClient(self.ssh_client.get_transport()) as scp:
+                scp.get(remote_path, local_path)
+        except SCPException:
+            raise SCPException.message
 
     def send_command(self, command):
+        """Send a single command"""
         stdin, stdout, stderr = self.ssh_client.exec_command(command)
-        exec_out = stdout.readlines()
-        print(exec_out)
+        return stdout.readlines()
 
 if __name__ == "__main__":
     ssh_manager = SSHManager()
