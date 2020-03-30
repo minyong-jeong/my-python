44ec0b7d4244e1c5a0f8c9d9963f994aaaf2a998
Minyong Jeong <jmy3155@gmail.com>
2020-03-30 15:15:00 +0900

    Add flask_session.py

---
+++ flask_session.py
@@ -0,0 +1,46 @@
+from datetime import timedelta
+from flask import Flask, session, redirect, url_for, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes.
+app.secret_key = b'SeCrEt_KeY'
+
+# Set session expiration.
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return '''
+        <p>Logged in as %s</p>
+        <a href="/logout">Logout!</p>
+        ''' % escape(session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return '''
+        <p>Logged out!!</p>
+        <a href="/">Go home!</a>
+    '''
+
+if __name__ ==  "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
