4e366acd9447966a651a5dab76d5ffca9374856b
Minyong Jeong <jmy3155@gmail.com>
2021-01-21 01:58:40 +0900

    Update rock_scissors_paper.py

--- rock_scissors_paper.py
+++ rock_scissors_paper.py
@@ -1,31 +1,27 @@
 import random
 
+
 def rock_scissors_paper():
     rsp_list = ['rock', 'scissors', 'paper']
-    
-    user_input = input("Please enter your choice(rock, scissors, paper): ").lower()
+
+    user_input = input(
+        "Please enter your choice(rock, scissors, paper): ").lower()
     if user_input not in rsp_list:
         print('[ERR] Input is incorrect')
         return
 
     computer_input = random.choice(rsp_list)
- 
-    user_index = rsp_list.index(user_input)
-    computer_index = rsp_list.index(computer_input)
-    
-    num = user_index - computer_index
-    if num < 0: num = num + 3
-
-    result = ''
-    if num == 0:
-        result = 'DRAW!!'
-    elif num == 1:
-        result = 'COM WIN!!'
-    elif num == 2:
-        result = 'USER WIN!!'
 
     print("USER (%s) <===> (%s) COM" % (user_input, computer_input))
-    print(result)
+    if user_input == computer_input:
+        print("DRAW!")
+    elif (user_input == "rock" and computer_input == "scissors") \
+            or (user_input == "scissors" and computer_input == "paper") \
+            or (user_input == "paper" and computer_input == "rock"):
+        print("USER WIN!")
+    else:
+        print("Com WIN!")
+
 
 if __name__ == "__main__":
     rock_scissors_paper()
