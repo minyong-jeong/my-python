306128a763700a9dfa444dfc86325117fffda08f
Minyong Jeong <jmy3155@gmail.com>
2021-02-01 13:57:18 +0900

    Add unique_chars.py

---
+++ unique_chars.py
@@ -0,0 +1,10 @@
+def has_unique_chars(string):
+    if string is None:
+        return False
+    return len(set(string)) == len(string)
+
+
+print(has_unique_chars("ABCD"))
+
+# print(has_unique_chars("ABCD")) --> True
+# print(has_unique_chars("ABCDD")) --> False
