8dd25c44a90b14a4394bff75787a29c3612b17f0
Minyong Jeong <jmy3155@gmail.com>
2021-02-03 00:28:30 +0900

    Update json_util.py

--- json_util.py
+++ json_util.py
@@ -1,12 +1,28 @@
 import json
 
-json_data = '{ "p1": { "name": "chulsu", "age": 20 }, "p2": { "name": "younghee", "age": 22 } }'
-dict_data = { "p1": { "name": "chulsu", "age": 20 }, "p2": { "name": "younghee", "age": 22 } }
 
-# Convert json to dict
-result = json.loads(json_data)
-print("parse_json result: %s" % type(result))
+class JsonUtil:
+    def convert_json_text_to_dict(self, text):
+        return json.loads(text)
 
-# Convert dict to json
-result = json.dumps(dict_data)
-print("convert_json result: %s" % type(result))
+    def convert_json_file_to_dict(self, filepath):
+        with open(filepath) as f:
+            return json.load(f)
+
+    def convert_dict_to_json_text(self, data):
+        return json.dumps(data)
+
+    def convert_dict_to_json_file(self, data, filepath):
+        with open(filepath, 'w') as f:
+            return json.dump(data, f)
+
+
+json_data = '{"p1": { "name": "A", "age": 20 }, "p2": { "name": "B", "age": 22 }}'
+dict_data = {"p1": {"name": "A", "age": 20}, "p2": {"name": "B", "age": 22}}
+
+ju = JsonUtil()
+result = ju.convert_json_text_to_dict(json_data)
+result = ju.convert_dict_to_json_text(dict_data)
+
+result = ju.convert_json_file_to_dict('./temp/sample.json')
+ju.convert_dict_to_json_file(dict_data, './temp/sample2.json')
